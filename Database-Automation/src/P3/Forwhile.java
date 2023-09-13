a15dd5af299354c93c62e145afde2c78768ef970
hkvkmech1 <hkvkmech1@gmail.com>
2023-09-13 13:11:15 +0530

    Recursion Method

---
+++ Database-Automation/src/P3/Forwhile.java
@@ -0,0 +1,17 @@
+package P3;
+
+public class Forwhile {
+	public static void main(String[] args) {
+		   {
+			int count=0;
+			for (int i=300;i>=100;i--)
+			  {
+			  if(i%2==0)
+			     {
+			     count=count+1;
+			     }
+			   }
+			  System.out.println(count);
+		   }
+		}
+	}
