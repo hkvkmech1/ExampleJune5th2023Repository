5b5a73b6808bddb9659fa993795535c1882655b5
hkvkmech1 <hkvkmech1@gmail.com>
2023-09-13 13:20:18 +0530

    Recursion Method

---
+++ Database-Automation/src/P4/Whileloop.java
@@ -0,0 +1,12 @@
+package P4;
+public class Whileloop {
+	  public static void main(String args[])
+	  {
+	    int i=1;
+	    while(i<=10)
+	    {
+	     System.out.println(15 + " * " + i + " = " + (15*i));
+	     i++;
+	    }
+	  }
+	}
